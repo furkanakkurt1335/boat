b1137cfb2008d817592674a6fd4d9c2555d8b1ac
salihfurqan <71407287+salihfurqan@users.noreply.github.com>
2022-03-06 21:53:35 +0300

    Latest script

--- src/main.js
+++ src/main.js
@@ -20,6 +20,10 @@ function button_handle(type) {
 			inject_sentence(sentences[current_sentence_index]);
 		}
 	}
+	else if (type == "col_add_rm_button") {
+		let sel = document.getElementById("col_add_rm_select");
+		if (sel.selectedIndex != 0) column_change(sel.options[sel.selectedIndex].text);
+	}
 	else if (type == "save") {
 		var content = "";
 		var sentences_len_t = sentences.length;
@@ -27,9 +31,7 @@ function button_handle(type) {
 			var sentence_t = sentences[i];
 			content += "# sent_id = " + sentence_t["sent_id"] + "\n";
 			content += "# text = " + sentence_t["text"] + "\n";
-			var parts_t = sentence_t["parts"];
-			var parts_len_t = parts_t.length;
-			for (let j = 0; j < parts_len_t; j++) {
+			for (let j = 0; j < sentence_t.length-2; j++) { // -2 for sent_id & text
 				var cols_t = parts_t[j];
 				var cols_len_t = cols_t.length;
 				for (let k = 0; k < cols_len_t; k++) {
@@ -57,121 +59,198 @@ function button_handle(type) {
 	}
 }
 
-// On input file load
-document.getElementById("inputfile").addEventListener("change", function() {
-	var fr = new FileReader();
-	fr.readAsText(this.files[0]);
-	fr.onload = function() {
-		document.getElementById("inputfile").remove();
-		document.getElementById("load-file").remove();
-
-		const previous_button = document.createElement("button");
-		previous_button.id = "previous";
-		previous_button.innerHTML = "Previous";
-		document.body.append(previous_button);
-		
-		const reset_button = document.createElement("button");
-		reset_button.id = "reset";
-		reset_button.innerHTML = "Reset";
-		document.body.append(reset_button);
-
-		const row_select_input = document.createElement("input");
-		row_select_input.type = "number";
-		row_select_input.id = "row_select";
-		document.body.append(row_select_input);
-
-		const row_select_select = document.createElement("select");
-		row_select_select.id = "row_select";
-		var row_select_options = ["Go to sentence", "Add row", "Remove row"];
-		for (let i = 0; i < row_select_options.length; i++) {
-			var row_select_option = document.createElement("option");
-			row_select_option.innerHTML = row_select_options[i];
-			row_select_select.append(row_select_option);
-		}
-		document.body.append(row_select_select);
-
-		const do_button = document.createElement("button");
-		do_button.id = "do";
-		do_button.innerHTML = "Do";
-		document.body.append(do_button);
-
-
-		const col_add_rm_select = document.createElement("select");
-		col_add_rm_select.id = "col_add_rm";
-		var col_sel_placeholder = document.createElement("option");
-		col_sel_placeholder.disabled = true;
-		col_sel_placeholder.selected = true;
-		col_sel_placeholder.innerHTML = "Select Column to add or remove";
-		col_add_rm_select.append(col_sel_placeholder);
-		var column_options = cats.concat(features);
-		for (let i = 0; i < column_options.length; i++) {
-			var column_option = document.createElement("option");
-			column_option.innerHTML = column_options[i];
-			col_add_rm_select.append(column_option);
-		}
-		col_add_rm_select.addEventListener("change", (event) => {
-			column_change(event.target.value);
-		});
-		document.body.append(col_add_rm_select);
-
-		row_select_input.style = "margin: 5px";
-		row_select_select.style = "margin: 5px";
-		col_add_rm_select.style = "margin: 5px";
-
-		const next_button = document.createElement("button");
-		next_button.id = "next";
-		next_button.innerHTML = "Next";
-		document.body.append(next_button);
-
-		const save_button = document.createElement("button");
-		save_button.id = "save";
-		save_button.innerHTML = "Save";
-		document.body.append(save_button);
-
-		const buttons = document.getElementsByTagName("button");
-		for (let i = 0; i < buttons.length; i++) {
-			buttons[i].addEventListener("click", function() {
-				button_handle(buttons[i].id);
-			});
-			buttons[i].style = "margin: 5px";
-		}
-
-		let br = document.createElement("br");
-		br.id = "br";
-		document.body.append(br);
-
-		const p_text = document.createElement("p");
-		p_text.id = "sentence_text";
-		document.body.append(p_text);
-		document.body.append(br);
-
-		const table = document.createElement("table");
-		table.id = "sentence_table";
-		document.body.append(table);
-
-		current_columns = ["ID", "FORM", "LEMMA", "UPOS", "XPOS", "FEATS", "HEAD", "DEPREL", "DEPS", "MISC"];
-		parse_file(fr.result);
-		inject_sentence(sentences[0]);
+// Keyboard shortcuts
+document.onkeyup = function(e) {
+	if (e.key.toLowerCase() == "p" && e.altKey) {
+		button_handle("previous");
 	}
-})
+	else if (e.key.toLowerCase() == "r" && e.altKey) {
+		button_handle("reset");
+	}
+	else if (e.key.toLowerCase() == "d" && e.altKey) {
+		button_handle("do");
+	}
+	else if (e.key.toLowerCase() == "x" && e.altKey) {
+		document.getElementById("col_add_rm_select").focus();
+	}
+	else if (e.key.toLowerCase() == "c" && e.altKey) {
+		button_handle("col_add_rm_button");
+	}
+	else if (e.key.toLowerCase() == "n" && e.altKey) {
+		button_handle("next");
+	}
+	else if (e.key.toLowerCase() == "s" && e.altKey) {
+		button_handle("save");
+	}
+	else if (e.key.toLowerCase() == "t" && e.altKey) {
+		document.getElementById("table").focus();
+	}
+	else if (e.key == "ArrowUp" || e.key == "ArrowDown" || e.key == "ArrowLeft" || e.key == "ArrowRight") {
+		let act_el_id = document.activeElement.id;
+		let matches = act_el_id.match(/row:(\d*), column:(\d*)/);
+		if (matches.length == 3) {
+			let row = parseInt(matches[1]);
+			let column = parseInt(matches[2]);
+			
+			let form_count = document.getElementById("table").getElementsByTagName("tr").length-1;
+			if (e.key == "ArrowUp" && row != 0) {
+				document.getElementById(`row:${row-1}, column:${column}`).focus();
+			}
+			else if (e.key == "ArrowDown" && row != form_count-1) {
+				document.getElementById(`row:${row+1}, column:${column}`).focus();
+			}
+			else if (e.key == "ArrowRight" && column != current_columns.length-1) {
+				document.getElementById(`row:${row}, column:${column+1}`).focus();
+			}
+			else if (e.key == "ArrowLeft" && column != 0) {
+				document.getElementById(`row:${row}, column:${column-1}`).focus();
+			}
+		}
+	}
+};      
 
 function column_change(column_option) {
 	if (current_columns.includes(column_option)) current_columns.splice(current_columns.indexOf(column_option), 1);
 	else current_columns = current_columns.concat(column_option);
+	sort_columns();
 	inject_sentence(sentences[current_sentence_index]);
 }
 
-// document.getElementById("inputfile").addEventListener("change", read_file);
+function sort_columns() {
+	for (let i = 0; i < cats.length; i++) {
+		let cat_t = cats[i];
+		if (current_columns.includes(cat_t)) {
+			current_columns.splice(current_columns.indexOf(cat_t), 1);
+			current_columns = current_columns.concat(cat_t);
+		}
+	}
+	for (let i = 0; i < features.length; i++) {
+		let feat_t = features[i];
+		if (current_columns.includes(feat_t)) {
+			current_columns.splice(current_columns.indexOf(feat_t), 1);
+			current_columns = current_columns.concat(feat_t);
+		}
+	}
+}
 
-// function read_file() {
-// 	var fr = new FileReader();
-// 	fr.readAsText(this.files[0]);
-// 	fr.onload = init_page(fr.result);
-// }
 
-// function init_page(file_content) {
-// 	parse_file(file_content);
-// }
+// On input file load
+document.getElementById("inputfile").addEventListener("change", read_file);
+
+function read_file(event) {
+	var fr = new FileReader();
+	fr.readAsText(this.files[0]);
+	fr.onload = event => {
+		init_page(fr.result);
+	};
+}
+
+function init_page(file_content) {
+	document.getElementById("load-file").remove();
+
+	var element_splitter = document.createElement("span");
+	element_splitter.innerHTML = "|";
+	element_splitter.style = "font-size: large; margin-left: 5px; margin-right: 5px";
+
+	const previous_button = document.createElement("button");
+	previous_button.id = "previous";
+	previous_button.innerHTML = "Previous";
+	document.body.append(previous_button);
+	
+	document.body.append(element_splitter.cloneNode(true));
+
+	const reset_button = document.createElement("button");
+	reset_button.id = "reset";
+	reset_button.innerHTML = "Reset";
+	document.body.append(reset_button);
+
+	document.body.append(element_splitter.cloneNode(true));
+
+	const row_select_input = document.createElement("input");
+	row_select_input.type = "number";
+	row_select_input.id = "row_select";
+	document.body.append(row_select_input);
+
+	const row_select_select = document.createElement("select");
+	row_select_select.id = "row_select";
+	var row_select_options = ["Go to sentence", "Add row", "Remove row"];
+	for (let i = 0; i < row_select_options.length; i++) {
+		var row_select_option = document.createElement("option");
+		row_select_option.innerHTML = row_select_options[i];
+		row_select_select.append(row_select_option);
+	}
+	document.body.append(row_select_select);
+
+	const do_button = document.createElement("button");
+	do_button.id = "do";
+	do_button.innerHTML = "Do";
+	document.body.append(do_button);
+
+	document.body.append(element_splitter.cloneNode(true));
+
+	const col_add_rm_select = document.createElement("select");
+	col_add_rm_select.id = "col_add_rm_select";
+	var col_sel_placeholder = document.createElement("option");
+	col_sel_placeholder.disabled = true;
+	col_sel_placeholder.selected = true;
+	col_sel_placeholder.innerHTML = "Select Column to add or remove";
+	col_add_rm_select.append(col_sel_placeholder);
+	var column_options = cats.concat(features);
+	for (let i = 0; i < column_options.length; i++) {
+		var column_option = document.createElement("option");
+		column_option.innerHTML = column_options[i];
+		col_add_rm_select.append(column_option);
+	}
+	document.body.append(col_add_rm_select);
+
+	row_select_input.style = "margin: 5px";
+	row_select_select.style = "margin: 5px";
+	col_add_rm_select.style = "margin: 5px";
+
+	const col_add_rm_button = document.createElement("button");
+	col_add_rm_button.id = "col_add_rm_button";
+	col_add_rm_button.innerHTML = "Add / Remove column selected";
+	document.body.append(col_add_rm_button);
+
+	document.body.append(element_splitter.cloneNode(true));
+
+	const next_button = document.createElement("button");
+	next_button.id = "next";
+	next_button.innerHTML = "Next";
+	document.body.append(next_button);
+
+	document.body.append(element_splitter.cloneNode(true));
+
+	const save_button = document.createElement("button");
+	save_button.id = "save";
+	save_button.innerHTML = "Save";
+	document.body.append(save_button);
+
+	const buttons = document.getElementsByTagName("button");
+	for (let i = 0; i < buttons.length; i++) {
+		buttons[i].addEventListener("click", function() {
+			button_handle(buttons[i].id);
+		});
+		buttons[i].style = "margin: 5px";
+	}
+
+	let br = document.createElement("br");
+	br.id = "br";
+	document.body.append(br.cloneNode(true));
+
+	const p_text = document.createElement("p");
+	p_text.id = "sentence_text";
+	document.body.append(p_text);
+	document.body.append(br);
+
+	const table = document.createElement("table");
+	table.id = "sentence_table";
+	document.body.append(table);
+
+	current_columns = ["ID", "FORM", "LEMMA", "UPOS", "XPOS", "FEATS", "HEAD", "DEPREL", "DEPS", "MISC"];
+	parse_file(file_content);
+	inject_sentence(sentences[0]);
+}
 
 var sentences = [];
 function parse_file(text) {
@@ -212,6 +291,7 @@ function parse_file(text) {
 
 var cats = ["ID", "FORM", "LEMMA", "UPOS", "XPOS", "FEATS", "HEAD", "DEPREL", "DEPS", "MISC"];
 var features = ["Abbr", "Animacy", "Aspect", "Case", "Clusivity", "Definite", "Degree", "Evident", "Foreign", "Gender", "Mood", "NounClass", "Number", "NumType", "Person", "Polarity", "Polite", "Poss", "PronType", "Reflex", "Tense", "Typo", "VerbForm", "Voice"];
+const all_column_count = cats.length + features.length;
 
 function create_table() {
 
@@ -279,13 +359,21 @@ function inject_sentence(sentence) {
 			let data = document.createElement("td");
 			if (sentence[i][current_columns[j]] == undefined) data.innerHTML = "_";
 			else data.innerHTML = sentence[i][current_columns[j]];
+			data.id = `row:${i}, column:${j}`;
 			data.style.textAlign = "center";
 			data.contentEditable = "true";
+			data.addEventListener("input", (event) => {
+				cell_change(i, j, event.target.innerHTML);
+			});
 			row.append(data);
 		}
 		tbody.append(row);
 	}
 	document.body.append(table);
+}
+
+function cell_change(form, column, cell) {
+	sentences[current_sentence_index][form][current_columns[column]] = cell;
 }
 
 function column_click(column_name) {
