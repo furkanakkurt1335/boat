073b11cbd6ac33a89b3ac9754a21d63efa71955c
salihfurqan <71407287+salihfurqan@users.noreply.github.com>
2022-03-18 12:01:41 +0300

    Change db to pg & Add new requirement

--- src/django_project/django_project/settings.py
+++ src/django_project/django_project/settings.py
@@ -74,10 +74,21 @@ WSGI_APPLICATION = 'django_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+import os
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'pg_db1',
+        'USER': os.environ.get('pg_db1_user'),
+        'PASSWORD': os.environ.get('pg_db1_password'),
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
