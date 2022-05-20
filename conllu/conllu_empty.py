3185ad45756e752823a43f9c74893a57d8c4dcb6
Furkan Akkurt <salihfurqan@gmail.com>
2022-05-20 22:04:48 +0300

    conllu

---
+++ conllu/conllu_empty.py
@@ -0,0 +1,14 @@
+
+data = open(r'C:\Users\salih\Dropbox\School\Current\CMPE492 - Project\CoNLL-U\annotator-agr, Sent by Busra, Original - Copy.conllu', 'r', encoding='utf-8').readlines()
+new_data = ''
+for line in data:
+    if not (line.startswith('#') or line == '' or '-' in line.split('\t')[0]):
+        tokens = line.split('\t')
+        new_tokens = list()
+        for i, token in enumerate(tokens): # deletes xpos, feats, head, deprel
+            if i in [4, 5, 6, 7]: new_tokens.append('_')
+            else: new_tokens.append(token)
+        new_line = '\t'.join(new_tokens)
+        new_data += f'{new_line}'
+    else: new_data += f'{line}'
+open('new_data.conllu', 'w', encoding='utf-8').write(new_data)
