42ebed3cbcc8d3cab927561e7f29d169a2e59957
Muhammet Şen <enmuhammet23@gmail.com>
2022-07-14 11:22:54 +0000

    Changed static files root path

--- docker/app/django_project/settings.py
+++ docker/app/django_project/settings.py
@@ -126,8 +126,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/boat/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+CSRF_TRUSTED_ORIGINS = ['http://79.123.177.252']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
